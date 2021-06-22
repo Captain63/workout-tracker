ebaa7990d87a3f4812b8dd93182225a984b27c8f
Captain63 <58921221+Captain63@users.noreply@github.com>
2021-06-22 17:46:11 -0400

    Add comments and remove extraneous route

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -1,18 +1,17 @@
 const path = require("path");
 
 const serveFiles = app => {
+    // Show homepage
     app.get("/", (req, res) => {
         res.sendFile(path.join(__dirname, "../public/index.html"));
     })
 
+    // Show exercise input form
     app.get("/exercise", (req, res) => {
         res.sendFile(path.join(__dirname, "../public/exercise.html"));
     })
 
-    app.get("/exercise?", (req, res) => {
-        res.sendFile(path.join(__dirname, "../public/exercise.html"));
-    })
-
+    // Show dashboard page
     app.get("/stats", (req, res) => {
         res.sendFile(path.join(__dirname, "../public/stats.html"));
     })
